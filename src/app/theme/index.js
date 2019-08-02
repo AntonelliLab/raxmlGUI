52ad62ebd9786a1a1a1f7f56b47d38343110a23d
Johannes Klein <johannes.t.klein@gmail.com>
2019-08-02 10:54:18 +0200

    Removed unused var

--- src/app/theme/index.js
+++ src/app/theme/index.js
@@ -1,7 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
-import indigo from '@material-ui/core/colors/indigo';
-import purple from '@material-ui/core/colors/purple';
 
 const theme = createMuiTheme({
   palette: {
