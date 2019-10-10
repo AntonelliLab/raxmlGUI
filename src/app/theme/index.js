174da9a4d41737ba59e8e0720a750450a3548a1f
Daniel Edler <edler.dev@gmail.com>
2019-10-10 12:27:36 +0200

    build: add mac disk image icon and replace react favicon

--- src/app/theme/index.js
+++ src/app/theme/index.js
@@ -1,7 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import amber from '@material-ui/core/colors/amber';
 import grey from '@material-ui/core/colors/grey';
-import teal from '@material-ui/core/colors/teal';
 
 const theme = createMuiTheme({
   palette: {
