de93e60a4fb54396b9525256b01d0fc618332dec
Johannes Klein <johannes.t.klein@gmail.com>
2019-12-02 11:31:20 +0100

    Add nucleotide models

---
+++ src/settings/raxmlng.js
@@ -0,0 +1,13 @@
+export const nucleotideSubstitutionModelOptions = {
+  options: [
+    'GTR',
+    'GTR+I',
+    'GTR+G',
+    'GTR+G+I',
+    'GTR+ASC_LEWIS',
+    'GTR+G+ASC_LEWIS'
+  ]
+};
+export const modelOptions = {
+  'nucleotide': nucleotideSubstitutionModelOptions,
+};
