8f5c3bc945be74d18d43cbe3aed589ab271bf9de
Johannes Klein <johannes.t.klein@gmail.com>
2019-12-30 14:17:55 +0100

    Bump standard raxml version to latest

--- src/app/store/Run.js
+++ src/app/store/Run.js
@@ -19,9 +19,9 @@ export const MAX_NUM_CPUS = cpus().length;
 const winBinaries = [
   // TODO: add raxml ng windows exe
   { name: 'raxmlHPC.exe', multithreaded: false, version: '8.2.10' },
-  // { name: 'raxmlHPC-SSE3.exe', multithreaded: false },
   { name: 'raxmlHPC-PTHREADS-AVX2.exe', multithreaded: true, version: '8.2.10' },
   { name: 'raxmlHPC-PTHREADS-SSE3.exe', multithreaded: true, version: '8.2.10' }
+  // { name: 'raxmlHPC-SSE3.exe', multithreaded: false, version: '8.2.12' },
 ];
 
 const binaries = util.is.windows
