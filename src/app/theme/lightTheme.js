7fa180e1ebfc719b26e49a465bfc2d77a52cb519
Johannes Klein <johannes.t.klein@gmail.com>
2020-01-18 09:41:44 +0100

    Revert "Update lightTheme.js"
    This reverts commit 462eabacffffa60294a1f3172cee10411bdb3e83.

--- src/app/theme/lightTheme.js
+++ src/app/theme/lightTheme.js
@@ -1,7 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import amber from '@material-ui/core/colors/amber';
 import grey from '@material-ui/core/colors/grey';
-import red from '@material-ui/core/colors/red';
 
 const lightTheme = createMuiTheme({
   palette: {
@@ -12,29 +11,46 @@ const lightTheme = createMuiTheme({
     },
     secondary: {
       main: grey[500],
+      // main: '#4E293A',
       contrastText: '#fff',
     },
+    // primary: amber,
+    // secondary: teal,
     model: {
-      light: 'hsla(332, 41%, 39%, 1)',
-      main: 'hsla(332, 41%, 33%, 1)',
-      dark: 'hsla(332, 41%, 27%, 1)',
-      darker: 'hsla(332, 41%, 21%, 1)',
-      secondaryText: 'hsla(332, 19%, 64%, 1)',
+      // light: '#6E495A',
+      light: 'hsla(332, 31%, 29%, 1)',
+      main: 'hsla(332, 31%, 23%, 1)',
+      // dark: '#2C0718',
+      dark: 'hsla(332, 31%, 17%, 1)',
+      // darker: '#1C0008',
+      darker: 'hsla(332, 31%, 11%, 1)',
+      secondaryText: 'hsla(332, 9%, 54%, 1)',
     },
     input: {
-      light: 'hsla(95, 37%, 39%, 1)',
-      main: 'hsla(95, 37%, 33%, 1)',
-      dark: 'hsla(95, 37%, 27%, 1)',
-      darker: 'hsla(95, 37%, 21%, 1)',
-      secondaryText: 'hsla(95, 19%, 64%, 1)',
+      // light: '#5B6E4D',
+      light: 'hsla(95, 27%, 29%, 1)',
+      // main: '#3B4E2D',
+      main: 'hsla(95, 27%, 23%, 1)',
+      // dark: '#192C0B',
+      dark: 'hsla(95, 27%, 17%, 1)',
+      // darker: '#091C00',
+      darker: 'hsla(95, 27%, 11%, 1)',
+      secondaryText: 'hsla(95, 9%, 54%, 1)',
     },
     output: {
-      light: 'hsla(211, 41%, 39%, 1)',
-      main: 'hsla(211, 41%, 33%, 1)',
-      dark: 'hsla(211, 41%, 27%, 1)',
-      darker: 'hsla(211, 41%, 21%, 1)',
+      // light: '#435262',
+      light: 'hsla(211, 31%, 29%, 1)',
+      // main: '#233242',
+      main: 'hsla(211, 31%, 23%, 1)',
+      // dark: '#011020',
+      dark: 'hsla(211, 31%, 17%, 1)',
+      // darker: '#000010',
+      darker: 'hsla(211, 31%, 11%, 1)',
     },
-    error: red,
+    error: amber,
+    // error: {
+    //   main: '#f2401b',
+    // },
     // Used by `getContrastText()` to maximize the contrast between the background and
     // the text.
     contrastThreshold: 3,
