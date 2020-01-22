dceb36e6fb1195614bf21743ac898dc254cd42e1
Johannes Klein <johannes.t.klein@gmail.com>
2020-01-22 12:47:58 +0100

    Notarization (#96)
    * Add dev dep
    
    * Add dotenv dependency
    
    * add entitlements
    
    * Add script to notarize app
    
    * Update README.md
    
    * Add dev dep
    
    * Add dotenv dependency
    
    * add entitlements
    
    * Add script to notarize app
    
    * Update README.md
    
    * Typo in entitlements file
    
    * Do not sign dmg
    
    * Bump builder
    
    * Add checks for env variable before notarizing
    
    * Refactor

---
+++ scripts/notarize.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appleId = process.env.APPLEID;
+  const appleIdPassword = process.env.APPLEIDPASS;
+  const ascProvider = process.env.APPLEIDTEAM;
+
+  if (!appleId) {
+    console.log(
+      'Johannes: Skipping notarization of app because no AppleID is given.'
+    );
+    console.log('Johannes: This binary is not ready for release.');
+    return;
+  }
+  if (!appleIdPassword) {
+    console.log(
+      'Johannes: Skipping notarization of app because no AppleID password is given.'
+    );
+    console.log('Johannes: This binary is not ready for release.');
+    return;
+  }
+  if (!ascProvider) {
+    console.log(
+      'Johannes: Skipping notarization of app because no AppleID team number is given.'
+    );
+    console.log('Johannes: This binary is not ready for release.');
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  console.log(`Notarizing ${appName} found at ${appOutDir}`);
+
+  return await notarize({
+    appBundleId: 'org.jtklein.raxmlGUI2',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId,
+    appleIdPassword,
+    ascProvider,
+  });
+};
