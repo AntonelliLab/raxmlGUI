03d5c2a58cfba92c8c3445248ffadbc19e07e7dc
Daniel Edler <edler.dev@gmail.com>
2020-04-14 11:22:25 +0200

    fix: Update url to download binaries

--- scripts/download-binaries.js
+++ scripts/download-binaries.js
@@ -13,7 +13,7 @@ const osName = (() => {
   }
 })();
 
-const binariesBaseDir = "https://github.com/AntonelliLab/raxmlGUI/releases/download/binaries-20.04/";
+const binariesBaseDir = "https://github.com/AntonelliLab/raxmlGUI-binaries/releases/download/v20.04/";
 const binariesUrl = `${binariesBaseDir}/${osName}.zip`;
 
 const binPath = path.join(__dirname, "..", "static", "bin");
