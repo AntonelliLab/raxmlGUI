37c5aa5730ecc83dc6614420965f3640e10121df
Johannes Klein <johannes.t.klein@gmail.com>
2020-07-13 12:51:26 +0200

    Add developer option to force an error thrown in electron side

--- src/main/menu/subMenuDeveloper.js
+++ src/main/menu/subMenuDeveloper.js
@@ -2,6 +2,13 @@ const subMenuDeveloper = {
   // If you see this menu in production sth is wrong
   label: 'DEVELOPER!!!',
   submenu: [
+    {
+      label: 'Force Error',
+      click() {
+        throw new Error('Testing Error');
+      }
+    },
+    { type: 'separator' },
     { role: 'toggledevtools' },
     { role: 'reload' },
     { role: 'forcereload' },
