2e1066b2ad3f33c4d4191bbcb8d521b95632d08a
Johannes Klein <johannes.t.klein@gmail.com>
2020-09-05 18:13:31 +0200

    Bump dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'mobx-react-lite/batchingForReactDom';
 import './index.css';
 import App from './app/';
 import registerServiceWorker from './registerServiceWorker';
