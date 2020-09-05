4eec8d8d0da16d490065ff1e8c1be02c923058d9
Johannes Klein <johannes.t.klein@gmail.com>
2020-09-05 11:53:00 +0200

    Download newer binaries with modeltest-ng included on Mac, Linux

--- scripts/download-binaries.js
+++ scripts/download-binaries.js
@@ -13,7 +13,7 @@ const osName = (() => {
   }
 })();
 
-const binariesBaseDir = "https://github.com/AntonelliLab/raxmlGUI-binaries/releases/download/v20.04";
+const binariesBaseDir = "https://github.com/AntonelliLab/raxmlGUI-binaries/releases/download/v20.09";
 const binariesUrl = `${binariesBaseDir}/${osName}.zip`;
 
 const binPath = path.join(__dirname, "..", "static", "bin");
