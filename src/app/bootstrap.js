6902c3d9112ea1ce9f41a9bb5341d0d597c0f9d4
Johannes Klein <johannes.t.klein@gmail.com>
2021-02-18 20:38:43 +0100

    Update bootstrap.js

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -20,14 +20,14 @@ function initDev(exampleFiles) {
   const exampleFilesDir = exampleFiles.dir;
   // const useFiles = exampleFiles.filter(file => exampleFilenames.includes(file.name));
   const useFastaFiles = [
-    'aminoacid.txt',
+    // 'aminoacid.txt',
     // 'bin1.txt',
     // 'bin2.txt',
     // 'binary.txt',
     // 'dna.txt',
     // 'mixed_data.txt',
     // 'multistate.txt',
-    // 'nucleotide.txt',
+    'nucleotide.txt',
   ].map(filename => ({ path: path.join(exampleFilesDir, 'fasta', filename) }));
   const usePhylipFiles = [
     // 'AA.txt',
