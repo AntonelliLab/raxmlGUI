a9cca76283169bc3f30e1dc9508752401f039007
Johannes Klein <johannes.t.klein@gmail.com>
2024-09-30 21:31:00 +0200

    Unused code

--- src/app/components/LoadingButton.js
+++ src/app/components/LoadingButton.js
@@ -1,48 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
-
-const useStyles = makeStyles(theme => ({
-  wrapper: {
-    position: 'relative',
-  },
-  progress: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12,
-    pointerEvents: 'none',
-  },
-}));
+import { Box } from '@mui/material';
 
 function LoadingButton(props) {
-  const classes = useStyles();
-  const { loading, className, ...buttonProps } = props;
+  const { loading, ...buttonProps } = props;
   const disabled = (loading || buttonProps.disabled);
 
   return (
-    <div className={clsx(classes.wrapper, className)}>
+    <Box sx={{ position: 'relative' }}>
       <Button
         variant="contained"
         disabled={disabled}
-        classes={{
-          root: classes.primaryButton,
-        }}
         {...buttonProps}
       >
-        { props.children }
+        {props.children}
       </Button>
-      {loading && (
-        <CircularProgress
-          size={24}
-          className={classes.progress}
-        />
-      )}
-    </div>
+      {loading && <CircularProgress size={24} sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+        pointerEvents: 'none',
+      }}
+      />}
+    </Box>
   );
 }
 
