4e35f35adabc91bec32e6368f4583865fa52abd8
Johannes Klein <johannes.t.klein@gmail.com>
2025-07-14 15:59:57 +0200

    Update CodeHighlight.js

--- src/app/components/CodeHighlight.js
+++ src/app/components/CodeHighlight.js
@@ -1,25 +1,15 @@
 import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/material';
 import Prism from 'prismjs';
 import 'prismjs-bibtex';
 import 'prismjs/themes/prism.css';
 import 'prismjs/themes/prism-okaidia.css'; // coy, dark, funky, okaidia, solarizedlight, tomorrow, twilight
 
-const useStyles = makeStyles(() => ({
-  code: {
-    fontSize: 10,
-    overflowWrap: 'break-word',
-    whiteSpace: 'pre-wrap',
-  }
-}));
-
 function CodeHighlight({ code, language, className }) {
   const codeNode = useRef(null);
-  const classes = useStyles();
 
   useEffect(() => {
-    // const code = codeNode.current.textContent;
     if (Prism.languages.hasOwnProperty(language)) {
       const highlightHTML = Prism.highlight(code, Prism.languages[language], language);
       codeNode.current.innerHTML = highlightHTML;
@@ -27,11 +17,19 @@ function CodeHighlight({ code, language, className }) {
   }, [code, language]);
 
   return (
-    <pre className={className}>
-      <code ref={codeNode} className={classes.code}>
+    <Box component="pre" className={className}>
+      <Box
+        component="code"
+        ref={codeNode}
+        sx={{
+          fontSize: 10,
+          overflowWrap: 'break-word',
+          whiteSpace: 'pre-wrap',
+        }}
+      >
         { code }
-      </code>
-    </pre>
+      </Box>
+    </Box>
   );
 }
 
