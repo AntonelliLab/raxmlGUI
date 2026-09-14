3fa909f57b35a76cca9fc77eea159dd13e741302
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-14 12:40:43 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,19 @@ module.exports = {
   // other webpack configuration
   devtool: 'source-map',
   plugins: [
-    new SentryWebpackPlugin({
-      // sentry-cli configuration - can also be done directly through sentry-cli
-      // see https://docs.sentry.io/product/cli/configuration/ for details
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      release: `${package.productName}@${package.version}`,
+    process.env.SENTRY_ORG && process.env.SENTRY_AUTH_TOKEN
+      ? new SentryWebpackPlugin({
+          // sentry-cli configuration - can also be done directly through sentry-cli
+          // see https://docs.sentry.io/product/cli/configuration/ for details
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          org: process.env.SENTRY_ORG,
+          project: process.env.SENTRY_PROJECT,
+          release: `${package.productName}@${package.version}`,
 
-      // other SentryWebpackPlugin configuration
-      include: '.',
-      ignore: ['node_modules', 'webpack.config.js'],
-    }),
-  ],
+          // other SentryWebpackPlugin configuration
+          include: '.',
+          ignore: ['node_modules', 'webpack.config.js'],
+        })
+      : null,
+  ].filter(Boolean),
 };
